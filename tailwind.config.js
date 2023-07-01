226185585475bb4243082be4f6f1d2854aea58a3
yogagalang <yogagalang609@gmail.com>
2023-07-01 23:03:15 +0700

    3-column preview card component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        display: ['Big Shoulders Display', 'cursive'],
+        sans: ['Lexend Deca', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
